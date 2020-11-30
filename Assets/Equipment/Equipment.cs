54be7928662fec338e54c196a614e2fc0ad9abd3
Marc Towler <marc@marctowler.co.uk>
2020-11-30 01:08:55 +0000

    misc update

---
+++ Assets/Equipment/Equipment.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Equipment : ScriptableObject
+{
+    public string itemName;
+    public string description;
+    public Sprite icon;
+
+    public virtual void Use()
+    {
+        
+    }
+}
